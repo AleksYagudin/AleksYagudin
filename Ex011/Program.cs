e36a7ce234968790481365df2b87cd2c8048cec5
AleksYagudin <aleksyagudin@icloud.com>
2022-08-21 19:08:07 +0300

    Исправленная домашняя работа

--- Ex011/Program.cs
+++ Ex011/Program.cs
@@ -6,14 +6,9 @@ int num1 = num * 0;
 while (num1 <= num)
 {
     
-    if (num1 % 2 ==0)
+    if (num1 % 2 == 0)
     {
     Console.WriteLine(num1);
     }
-    else
-    {
-    
-    }
-   
     num1++;
 }
