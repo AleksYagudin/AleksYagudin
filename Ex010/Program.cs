eba4798a5859d325f05baa7933245cd435d5420b
AleksYagudin <aleksyagudin@icloud.com>
2022-08-21 11:01:29 +0300

    3 домашняя работа

---
+++ Ex010/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a % 2 == 0 )  // Смотри на остаток от деления, если равен 0 то все ок!
+{
+Console.WriteLine("Четное ");
+}
+else
+{
+Console.WriteLine("Не четное");
+}
+
+
