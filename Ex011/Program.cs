4faa9f07b36f6d28e1753f4d1740f28ab14752ed
AleksYagudin <aleksyagudin@icloud.com>
2022-08-21 12:09:49 +0300

    4 домашняя работа

---
+++ Ex011/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Number enter : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num * 0;
+
+while (num1 <= num)
+{
+    
+    if (num1 % 2 ==0)
+    {
+    Console.WriteLine(num1);
+    }
+    else
+    {
+    
+    }
+   
+    num1++;
+}
