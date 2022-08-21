7c8c9011b29126a571d8fe4fc84cbd999eff57b3
AleksYagudin <aleksyagudin@icloud.com>
2022-08-21 09:23:21 +0300

    1 домашняя работа

---
+++ Ex008/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
