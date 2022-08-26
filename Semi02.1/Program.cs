5d226388ec5ac677d710049fb66043d83d998ee2
AleksYagudin <aleksyagudin@icloud.com>
2022-08-26 19:29:18 +0300

    задача с вычетом цифры в середине

---
+++ Semi02.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задачи : Случайное 3х значное число и вывести результат без цифры в середине
+
+Random random = new Random();
+int randomNumber = random.Next(100,1000);
+
+Console.WriteLine("Выпавшее число " + randomNumber);
+int lastNumber = randomNumber / 100;  
+int firstNumber = randomNumber % 10;
+
+Console.Write($"Число без цифры в середин = {lastNumber}{firstNumber}");
