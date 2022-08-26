c3f11e09634b8a01a7b2bb9ad17b36bf05dc025a
AleksYagudin <aleksyagudin@icloud.com>
2022-08-26 19:50:44 +0300

    задача на кратность

---
+++ Semi02.2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int firstNumber = Convert.ToInt32(Console.ReadLine()); //Вводим число на которое будем делить
+int twoNumber = Convert.ToInt32(Console.ReadLine());  //Вводим число которое делим
+
+if (twoNumber % firstNumber == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    int number = (twoNumber % firstNumber);
+    Console.WriteLine($"Не кратно, остаток : {number}");
+}
+
