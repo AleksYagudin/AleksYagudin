0cd327846a0825ead42278a9014db41975022b96
AleksYagudin <aleksyagudin@icloud.com>
2022-08-26 20:16:19 +0300

    первый метод

---
+++ Semi02.3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Random random = new Random(); 
+int randomNumber = random.Next(10,100); 
+Console.WriteLine("Выпавшее число " + randomNumber);
+
+int GetMaxNumber(int number)  // Метод GetMaxNumber ищет в случайном 2х
+                              //значном чмисле, большее из этих 2х.
+{
+    int lastNumber = number % 10;
+    int firstNumber = number / 10;
+    if (lastNumber > firstNumber)
+    {
+        return lastNumber;
+    }
+   else
+    {
+        return firstNumber;
+    }
+}
+int result = GetMaxNumber(randomNumber);
+
+Console.WriteLine(result);
